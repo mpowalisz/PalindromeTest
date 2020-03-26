9853154f4968b5531064808969e31d85d069502e
mpowalisz <markpowalisz@gmail.com>
2020-03-26 15:38:50 -0400

    Changed input value to a placeholder. Removed console.log

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,12 @@
-
 function palindromeTest() {
-  let string = document.getElementById("palindrome").value
-  console.log(string);
-  document.getElementById("input").innerHTML = "Is " + string + " a palindrome?"
-	let newPhrase = string.toLowerCase().replace(/\W/g, '')
-	let reversedPhrase = newPhrase.split("").reverse().join("");
-  let y = (newPhrase === reversedPhrase)? "Yes" : "No";
+  let string = document.getElementById("palindrome").value;
+  document.getElementById("input").innerHTML =
+    'Is "' + string + '" a palindrome?';
+  let newPhrase = string.toLowerCase().replace(/\W/g, "");
+  let reversedPhrase = newPhrase
+    .split("")
+    .reverse()
+    .join("");
+  let y = newPhrase === reversedPhrase ? "Yes" : "No";
   document.getElementById("result").innerHTML = y;
 }
